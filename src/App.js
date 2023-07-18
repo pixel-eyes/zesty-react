34bd039c13dbb136b2d20416d05a34b14052c713
Rachel Li <rachellee5127@gmail.com>
2023-07-18 17:51:19 +1000

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react";
+import cabbage from './assets/image1.jpeg'
+import mango from './assets/image2.jpeg'
+import fig from './assets/image3.jpeg'
+import gaze from './assets/image4.jpeg'
 
-function App() {
+const images = [ cabbage, mango, fig, gaze ];
+
+const App = () => {
+  const [currentImage, setCurrentImage] = useState (0);
+  
+  const handClick = () => {
+    const length = images.length - 1;
+
+    setCurrentImage ((currentImage) => {
+      if (currentImage < length) {
+      return currentImage + 1; 
+      } else {
+        return 0;
+      }
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="title">
+        <h1>Zesty</h1>
+        <h2>A photography project <br/>by Ella
+        Fieldling
+        </h2>
+        <h3>
+          {currentImage + 1} / {images.length}
+        </h3>
+      </div>
+      <div className="image-container">
+        <img alt="" src={images[currentImage]} onClick={handClick} />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
